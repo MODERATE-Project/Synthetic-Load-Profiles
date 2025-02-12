c2ebbbc8c34e16a12e7cba2e4d69e1cbfb78bc8f
PhilippMaschi <phil.masch@hotmail.com>
2025-02-12 14:10:08 +0100

    changed start params

--- start_without_GUI.py
+++ start_without_GUI.py
@@ -10,19 +10,19 @@ from main import run
 PROJECT_NAME = 'test'
 
 # Input file path
-INPUT_PATH = Path.cwd() / 'data' / 'sample_data' / 'opendata_fluvius' / 'P6269_1_50_DMK_Sample_Elek_Volume_Afname_kWh_HP_resampled.csv'
+INPUT_PATH = Path.cwd() / 'data' / 'sample_data' / 'smart_meters_london_resampled' / 'smart_meters_london_resampled.csv'
 
 # Output file format ('npy', 'csv' or 'xlsx')
 OUTPUT_FORMAT = '.npy'
 
 # Use Wandb (if True, metric will be tracked online; Wandb account required)
-USE_WANDB = False
+USE_WANDB = True
 
 # Set the number of epochs
 EPOCH_COUNT = 100
 
 # Change the result save frequency; save all samples/models in addition to visualizations
-SAVE_FREQ = 1000
+SAVE_FREQ = 50
 SAVE_SAMPLES = False
 SAVE_MODELS = False
 
@@ -30,7 +30,7 @@ SAVE_MODELS = False
 
 # Model state path (optional, for continuation of training or generation of data)
 MODEL_PATH = None
-MODEL_PATH = r'C:\Users\Arbeit\Projekte\Git\GAN\runs\test\2024_12_19_102947716\models\epoch_100\epoch_100.pt.gz'
+# MODEL_PATH = r'C:\Users\Arbeit\Projekte\Git\GAN\runs\test\2024_12_19_102947716\models\epoch_100\epoch_100.pt.gz'
 
 # Create synthetic data from existing model (if True, there is no training)
 CREATE_DATA = False
